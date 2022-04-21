0b36602ac62cc509c635d907280ce777fbb075f5
Vidal <vidies1997@gmail.com>
2022-04-21 20:40:53 +0200

    Upates CModuleRenderer to use OpenGL

--- src/CModuleRenderer.cpp
+++ src/CModuleRenderer.cpp
@@ -3,6 +3,9 @@
 #include "CApplication.h"
 #include "CModuleWindow.h"
 
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
 bool CModuleRenderer::Init()
 {
 	return true;
@@ -10,6 +13,71 @@ bool CModuleRenderer::Init()
 
 bool CModuleRenderer::Update()
 {
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		0.0f, 0.5f, 0.0f
+	};
+
+	const char* vertexShaderSource = "#version 330 core\n"
+		"layout (location = 0) in vec3 aPos;\n"
+		"void main()\n"
+		"{\n"
+		"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+		"}\0";
+
+	unsigned int vertexShader;
+	vertexShader = glCreateShader( GL_VERTEX_SHADER );
+	glShaderSource( vertexShader, 1, &vertexShaderSource, NULL );
+	glCompileShader( vertexShader );
+
+	int success;
+	glGetShaderiv( vertexShader, GL_COMPILE_STATUS, &success );
+	if( !success )
+		return false;
+
+	const char* fragmentShaderSource = "#version 330 core\n"
+		"out vec4 FragColor;\n"
+		"void main()\n"
+		"{\n"
+		"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0);\n"
+		"}\0";
+
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader( GL_FRAGMENT_SHADER );
+	glShaderSource( fragmentShader, 1, &fragmentShaderSource, NULL );
+	glCompileShader( fragmentShader );
+
+	glGetShaderiv( fragmentShader, GL_COMPILE_STATUS, &success );
+	if( !success )
+		return false;
+
+	const auto shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if(!success)
+		return false;
+
+	glDeleteShader( vertexShader );
+	glDeleteShader( fragmentShader );
+
+	unsigned int VBO;
+	glGenBuffers( 1, &VBO );
+	unsigned int VAO;
+	glGenVertexArrays( 1, &VAO );
+	glBindVertexArray( VAO );
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glUseProgram(shaderProgram);
+	glBindVertexArray(VAO);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+
 	return true;
 }
 
