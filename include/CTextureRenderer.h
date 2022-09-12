b0ce0ec1b587a428a7393c8eb1a6cdadd09cfd46
Vidal <vidies1997@gmail.com>
2022-09-12 16:00:35 +0200

    Creates CTextureRenderer

---
+++ include/CTextureRenderer.h
@@ -0,0 +1,34 @@
+#ifndef CTEXTURE_RENDERER__H
+#define CTEXTURE_RENDERER__H
+
+#include <string>
+#include <vector>
+
+class CTexture;
+
+class CTextureRenderer
+{
+public:
+	CTextureRenderer() = default;
+	CTextureRenderer( const std::string& aTexturePath );
+	~CTextureRenderer() = default;
+
+	unsigned int GetTextureId() const;
+	float GetTextureWidth() const;
+	float GetTextureHeight() const;
+	const std::string& GetTexturePath() const;
+
+	bool HasTexture() const;
+	bool AttachTexture( const std::string& aTexturePath );
+	void UpdateVerticesData();
+	void Render() const;
+
+private:
+	std::vector<float> mVertices;
+	CTexture* mTexture;
+	std::string mTexturePath;
+	unsigned int mVBO;
+	unsigned int mVAO;
+};
+
+#endif
